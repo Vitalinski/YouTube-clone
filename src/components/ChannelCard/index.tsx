5516c782af5b8c9a4b635d4dd475757fd09badcc
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2024-03-10 18:21:41 +0100

    Added ChannelCard component

--- src/components/ChannelCard/index.tsx
+++ src/components/ChannelCard/index.tsx
@@ -1,10 +1,75 @@
-import { FC } from 'react';
+import { FC } from "react";
+import {
+  Box,
+  CardComponent,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "@/utils/Constants";
 
-const ChannelCard: FC = () => {
+interface ChannelDetailProps {
+  channelDetail: {
+    statistics?:{
+      subscriberCount:string
+    }
+    id: {
+      channelId: string;
+    };
+    snippet: {
+      title: string;
+      thumbnails?: {
+        high?: {
+          url: string;
+        };
+      };
+      channelId?: string;
+      channelTitle?: string;
+    };
+  };
+}
+
+const ChannelCard: FC<ChannelDetailProps> = ({ channelDetail }) => {
   return (
-    <>
-    channel
-    </>
+    <Box sx={{display:'flex', justifyContent:'center', alignItems:'center',  boxShadow: "none", borderRadius: "20px", width:{xs:'356px', md:'320px'}, height:'326px', margin:'auto' }}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            component="img"
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              width: "180px",
+              height: "180px",
+              border: "2px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
   );
 };
 
--- src/components/VideoCard/index.tsx
+++ src/components/VideoCard/index.tsx
@@ -4,7 +4,6 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '@/utils/Constants';
 
-// Определение интерфейса для пропсов компонента VideoCard
 interface VideoCardProps {
   video: {
     id: {
@@ -29,13 +28,15 @@ const VideoCard: FC<VideoCardProps> = ({ video:{id:{videoId}, snippet} }) => {
     <Card sx={{ width: { md: '320px', xs: '100%', borderRadius: 0, boxShadow: 'none' } }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
+        component="img"
+        alt={snippet?.title}
           sx={{ width: 358, height: 180 }}
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant='subtitle1' fontWeight='bold' color='#FFF'>{snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}</Typography>
+          <Typography variant='subtitle1' fontWeight='bold' color='#FFF' >{snippet?.title.slice(0, 40) || demoVideoTitle.slice(0, 40)}</Typography>
         </Link>
         <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
           <Typography variant='subtitle2' fontWeight='bold' color='gray'>{snippet?.channelTitle || demoChannelTitle}
